90084a7d54f11db8d522132758b1719577764f6d
ChrisJFarr <selithey@gmail.com>
2017-12-26 15:34:42 -0600

    RobotDrive class start

---
+++ python_app/robot_drive/driving_functions/robot_drive/robot_drive.ino
@@ -0,0 +1,172 @@
+#include <Adafruit_MotorShield.h>
+#include <Wire.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+#include <stdlib.h>     /* abs */
+
+// Create the Adafruit_MotorShiel object
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+// Request the DC motor from the Adafruit_MotorShield
+Adafruit_DCMotor *frontLeftMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *backLeftMotor = AFMS.getMotor(2);
+Adafruit_DCMotor *frontRightMotor = AFMS.getMotor(3);
+Adafruit_DCMotor *backRightMotor = AFMS.getMotor(4);
+
+// Movement support/ communication
+int incoming[4];
+int wheelSpeed = 0;
+int travelDirection = 1;
+
+void setup() {
+  Serial.begin(9600); //start serial port
+  // put your setup code here, to run once:
+  // Call begin on the AFMS object
+  AFMS.begin();
+  // Set the default speed
+  setLeftSpeed(0);
+  setRightSpeed(0);
+}
+
+void loop() {
+
+  // Fill incoming array
+  fillIncoming();
+
+  // Set wheelSpeed with incoming[0] if there is a change
+  if (incoming[0] != wheelSpeed)
+  {
+    wheelSpeed = incoming[0];
+  }
+
+  // Check for rotation, else set driving direction
+  if (incoming[2] == 0 && incoming[3] == 2)
+  {
+    // If left = 0 and right = 2 then rotate left
+    rotateLeft();
+    setLeftSpeed(wheelSpeed);
+    setRightSpeed(wheelSpeed);
+  } else if (incoming[2] == 2 && incoming[3] == 0)
+  {
+    // If left = 2 and right = 0 then rotate right
+    rotateRight();
+    setLeftSpeed(wheelSpeed);
+    setRightSpeed(wheelSpeed);
+  } else
+  {
+    // Set default travel speed for both sides
+    setRightSpeed(wheelSpeed);
+    setLeftSpeed(wheelSpeed);
+
+    if (incoming[1] == 1) // Set driving direction
+    {
+      driveForward();
+    } else
+    {
+      driveBackward();
+    }
+  }
+
+  if (incoming[2] == 0 && incoming[3] == 1)
+  {
+    // If left = 0 and right = 1 then right is wheelSpeed, left is 0
+    setRightSpeed(wheelSpeed);
+    setLeftSpeed(0);
+  } else if (incoming[2] == 1 && incoming[3] == 0)
+  {
+    // If left = 1 and right = 0 then left is wheelSpeed, right is 0
+    setRightSpeed(0);
+    setLeftSpeed(wheelSpeed);
+  }
+  delay(50);
+}
+
+void fillIncoming()
+{
+  // Fill incoming array with Serial.read()
+  while (Serial.available() >= 4)
+  {
+    // fill array with loop
+    for (int i = 0; i < 4; i++)
+    {
+      incoming[i] = Serial.read();
+    }
+  }
+}
+
+
+void rotateLeft()
+{
+  // Set motor directions for a left rotation
+  frontLeftMotor->run(BACKWARD);
+  backLeftMotor->run(BACKWARD);
+  backRightMotor->run(FORWARD);
+  frontRightMotor->run(FORWARD);
+}
+
+void rotateRight()
+{
+  // Set motor directions for a right rotation
+  frontLeftMotor->run(FORWARD);
+  backLeftMotor->run(FORWARD);
+  backRightMotor->run(BACKWARD);
+  frontRightMotor->run(BACKWARD);
+}
+
+void driveForward()
+{
+  // Set motor directions for driving foward
+  frontLeftMotor->run(FORWARD);
+  backLeftMotor->run(FORWARD);
+  backRightMotor->run(FORWARD);
+  frontRightMotor->run(FORWARD);
+}
+
+void driveBackward()
+{
+  // Set motor directions for driving backward
+  frontLeftMotor->run(BACKWARD);
+  backLeftMotor->run(BACKWARD);
+  backRightMotor->run(BACKWARD);
+  frontRightMotor->run(BACKWARD);
+}
+
+void stopLeft()
+{
+  // Stop left side motors
+  frontLeftMotor->run(RELEASE);
+  backLeftMotor->run(RELEASE);
+}
+
+void stopRight()
+{
+  // Stop right side motors
+  frontRightMotor->run(RELEASE);
+  backRightMotor->run(RELEASE);
+}
+
+void setLeftSpeed(int newSpeed)
+{
+  // If newSpeed is 0, stop left side
+  if (newSpeed == 0)
+  {
+    stopLeft();
+  }
+  // Set left side speeds to newSpeed
+  frontLeftMotor->setSpeed(newSpeed);
+  backLeftMotor->setSpeed(newSpeed);
+
+}
+
+void setRightSpeed(int newSpeed)
+{
+  // If newSpeed is 0, stop right side
+  if (newSpeed == 0)
+  {
+    stopRight();
+  }
+  // Set right side speeds to newSpeed
+  frontRightMotor->setSpeed(newSpeed);
+  backRightMotor->setSpeed(newSpeed);
+
+}
+
--- python_app/robot_drive/working_file.py
+++ python_app/robot_drive/working_file.py
@@ -1,12 +1,115 @@
-# Next steps... get bowow to run from raspbpi
-# Connect to raspbpi from server
+## Standardizing movement of robot to a finite set of functions
+## Rotate: int input for degrees to turn
+## Drive: boolean forward input for direction and int distance for inches to drive
 
 
-# Feed video from pi camera to main comp through server
+# todo Syncing commands with robot_drive (permanent version of robot drive arduino code)
+# todo On initizializing, set default speed with
+# data.write(struct.pack('>B', default_speed))
 
-#
-# Get the robot controls working on raspberry pi same as the main computer
-# Set up the REST api and get basic communication working between computer and pi
+# todo Drive robot forward for x seconds and measure distance traveled
+# Full speed
+# Half speed
+
+# todo Rotate robot for x seconds and calculate degrees rotated
+# todo change speed and calculate again, try to predict movement for any speed assuming linear variations
+
+
+
+# Use keyboard control for driving robot
+# import serial
+# Linux
+import keyboard
+import serial
+import struct
+from time import sleep, time
+import argparse
+
+# Arduino input details:
+# struct.pack('>BBBB', speed, direction, turn[0], turn[1])
+# int speed: int of range(0, 256), determines current driving speed for forward or reverse
+# boolean direction: True drives forward, False drives backward
+# int list turn: 0, 1, or 2
+# when [0, 1], left turn while driving forward
+# [1, 0] right turn while driving forward
+# [0, 2] left spin, not driving
+# [2, 0] right spin, not driving
+
+# Driving input information
+# If up, then positive speed
+# If down, then negative speed
+# If neither, then 0 speed and 1 direction
+
+# If non-zero speed and left then turn = [0, 1]
+# If zero speed and left then turn = [0, 2]
+# If non-zero speed and right then turn = [1, 0]
+# If zero speed and right then turn = [2, 0]
+
+
+# Create robot drive class
+class RobotDrive:
+    
+    def __init__(self):
+        # Start the serial port to communicate with arduino
+        self.arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        # Keyboard control support
+        self.up = False
+        self.down = False
+        self.left = False
+        self.right = False
+        self.previous_speed = 0
+        self.previous_turn = [0, 0]
+    
+    def driveStraight(self, seconds: int):
+        start_time = time()
+        #while time() - start_time < 3:
+    
+    def keyboardControl(self, speed = 200):
+        defaultSpeed = speed
+        defaultTurn = [0, 0]
+        while True:
+            speed = defaultSpeed
+            turn = defaultTurn
+            # Loop to capture key strokes and write date using robot function
+            self.up = keyboard.is_pressed("Up") if not self.down else False
+            self.down = keyboard.is_pressed("Down") if not self.up else False
+            self.left = keyboard.is_pressed("Left") if not self.right else False
+            self.right = keyboard.is_pressed("Right") if not self.left else False
+            print([self.up, self.down, self.left, self.right])
+            
+            if self.up or self.down:
+                if self.left or self.right:
+                    turn = [int(not self.left), int(not self.right)]
+                if self.down:
+                    # Negative speed
+                    speed *= -1
+            elif self.left or self.right:
+                turn = [int(not self.left) * 2, int(not self.right) * 2]
+            else:
+                speed = 0
+            
+            # For troubleshooting/development
+            print([abs(speed), speed >= 0, turn[0], turn[1]])
+        
+            # If any changes, write new data to arduino
+            if self.previous_turn != turn or self.previous_speed != speed:
+                self.arduino.write(struct.pack('>BBBB', abs(speed), speed >= 0, turn[0], turn[1]))
+                self.previous_turn = turn
+                self.previous_speed = speed
+            
+            if keyboard.is_pressed("esc"):
+                break
+        
+            sleep(.05)
+
+        return True
+
+
+if __name__ == "__main__":
+    
+    rd = RobotDrive()
+    rd.keyboardControl()
+
 
 
 
