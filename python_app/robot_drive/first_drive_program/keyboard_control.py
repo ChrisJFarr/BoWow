796fe01b227095e540e8cc140dada6b7bf8f3ae8
ChrisJFarr <selithey@gmail.com>
2017-12-26 15:33:00 -0600

    Finalized first_drive_program

--- python_app/robot_drive/first_drive_program/keyboard_control.py
+++ python_app/robot_drive/first_drive_program/keyboard_control.py
@@ -6,18 +6,36 @@ import serial
 import struct
 from time import sleep
 
+# Arduino input details:
+# struct.pack('>BBBB', speed, direction, turn[0], turn[1])
+# int speed: int of range(0, 256), determines current driving speed for forward or reverse
+# boolean direction: True drives forward, False drives backward
+# int list turn: 0, 1, or 2
+# when [0, 1], left turn while driving forward
+# [1, 0] right turn while driving forward
+# [0, 2] left spin, not driving
+# [2, 0] right spin, not driving
+
+# Driving input information
+# If up, then positive speed
+# If down, then negative speed
+# If neither, then 0 speed and 1 direction
+
+# If non-zero speed and left then turn = [0, 1]
+# If zero speed and left then turn = [0, 2]
+# If non-zero speed and right then turn = [1, 0]
+# If zero speed and right then turn = [2, 0]
+
 # Start the serial port to communicate with arduino
 data = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
-# todo Turns should add a bit to one side and remove a bit from the other
-# todo Speed should be a constant level
-# todo Robot should stop when no buttons are pressed
-# todo Fix the turning inversion while driving
+# Set global arrow variables
 left = False
 up = False
 right = False
 down = False
 
+# Storing previous command information to avoid new duplicate commands
 previous_turn = [0, 0]
 previous_speed = 200
 
@@ -28,11 +46,14 @@ def manageRobot():
     turn = [0, 0]
 
     if up:
-        speed *= 1
+        # Positive speed
+        pass
     elif down:
+        # Negative speed
         speed *= -1
     else:
-        speed *= 0
+        # Zero speed
+        speed = 0
 
     if left:
         turn = [0, 1] if speed else [0, 2]
@@ -62,15 +83,8 @@ if __name__ == "__main__":
         if keyboard.is_pressed("esc"):
             break
         
-        sleep(.1)
+        sleep(.05)
 
 
-# If up, then positive speed
-# If down, then negative speed
-# If neither, then 0 speed and 1 direction
 
-# If non-zero speed and left then turn = [0, 1]
-# If zero speed and left then turn = [0, 2]
-# If non-zero speed and right then turn = [1, 0]
-# If zero speed and right then turn = [2, 0]
 
