63489cddc22a227eee2d6b77aa40a5d5d79375ef
ChrisJFarr <chris.farr@cerner.com>
2017-12-26 18:03:35 -0600

    Command line interface and distance function

--- python_app/robot_drive/working_file.py
+++ python_app/robot_drive/working_file.py
@@ -44,7 +44,6 @@ import argparse
 # If zero speed and left then turn = [0, 2]
 # If non-zero speed and right then turn = [1, 0]
 # If zero speed and right then turn = [2, 0]
-# test
 
 
 # Create robot drive class
@@ -53,6 +52,7 @@ class RobotDrive:
     def __init__(self):
         # Start the serial port to communicate with arduino
         self.arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        sleep(2)  # Must wait 2 seconds for arduino connection to fully initialize
         # Keyboard control support
         self.up = False
         self.down = False
@@ -61,13 +61,48 @@ class RobotDrive:
         self.previous_speed = 0
         self.previous_turn = [0, 0]
     
-    def driveStraight(self, seconds: int):
-        start_time = time()
-        #while time() - start_time < 3:
+    def sendInstructions(self, speed: int, turn: list):
+        self.arduino.write(struct.pack('>BBBB', abs(speed), speed >= 0, turn[0], turn[1]))
+        return None
+    
+    def driveStraight(self, seconds: float, speed: int):
+        print("seconds")
+        print(seconds)
+        print("speed")
+        print(speed)
+        turn = [0, 0]
+        #sleep(0)
+        # todo Send drive instructions to arduino
+        self.sendInstructions(speed, turn)
+        # todo Wait seconds
+        sleep(seconds)
+        # todo Send stop instructions to arduino
+        self.sendInstructions(0, turn)
+        
+        return None
+    
+    def driveDistance(self, distance: int, speed: int):
+        # distance is in inches
+        # convert 1/4 inch per second per 5 speed to seconds
+        seconds = distance / (speed * 0.068)  # Rough conversion to seconds using test data
+        
+        self.driveStraight(seconds, speed)
+        
+        return None
+    
+##    def rotateLeft(self, seconds: float, speed: int):
+##        turn = [0, 2]
+##        
+##        
+##    
+##    def rotateDegrees(self, degrees: int, speed: int):
+        
     
     def keyboardControl(self, speed = 200):
+        
         defaultSpeed = speed
         defaultTurn = [0, 0]
+        
         while True:
             speed = defaultSpeed
             turn = defaultTurn
@@ -76,7 +111,7 @@ class RobotDrive:
             self.down = keyboard.is_pressed("Down") if not self.up else False
             self.left = keyboard.is_pressed("Left") if not self.right else False
             self.right = keyboard.is_pressed("Right") if not self.left else False
-            print([self.up, self.down, self.left, self.right])
+            # print([self.up, self.down, self.left, self.right])
             
             if self.up or self.down:
                 if self.left or self.right:
@@ -90,11 +125,11 @@ class RobotDrive:
                 speed = 0
             
             # For troubleshooting/development
-            print([abs(speed), speed >= 0, turn[0], turn[1]])
+            # print([abs(speed), speed >= 0, turn[0], turn[1]])
         
             # If any changes, write new data to arduino
             if self.previous_turn != turn or self.previous_speed != speed:
-                self.arduino.write(struct.pack('>BBBB', abs(speed), speed >= 0, turn[0], turn[1]))
+                self.sendInstructions(speed, turn)
                 self.previous_turn = turn
                 self.previous_speed = speed
             
@@ -109,7 +144,23 @@ class RobotDrive:
 if __name__ == "__main__":
     
     rd = RobotDrive()
-    rd.keyboardControl()
+    
+    parser = argparse.ArgumentParser(description="Commands for robot")
+    parser.add_argument("-m", "--manual", action="store_true", help="Activate keyboard control with speed argument")
+    parser.add_argument("-t", "--time", type=int, metavar="", help="Set time in seconds")
+    parser.add_argument("-s", "--speed", type=int, help="Set speed between 0 and 255 inclusive")
+    parser.add_argument("-d", "--distance", type=float, help="Set driving distance in inches, requies also either time or speed")
+    
+    args = parser.parse_args()
+    
+    if args.manual and args.speed:
+        rd.keyboardControl(args.speed)
+    elif args.speed and args.time:
+        rd.driveStraight(args.time, args.speed)
+    elif args.speed and args.distance:
+        rd.driveDistance(args.distance, args.speed)
+    else:
+        parser.print_help()
 
 
 
