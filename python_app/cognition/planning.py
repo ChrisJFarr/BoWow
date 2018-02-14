2029fb57cdb5f8ac2bcad81db86a1ef6b8cf6a60
ChrisJFarr <chris.farr@cerner.com>
2018-02-13 21:20:34 -0600

    Final commit prior to restructure
    Moving contents out of python_app into main folder, archiving all other folders.

---
+++ python_app/cognition/planning.py
@@ -0,0 +1,18 @@
+"""
+
+How will it plan?
+
+Starting small...
+
+
+Develop basic A* planning api from scratch..
+Test on multiple types of problems? Randomly generated?
+
+"""
+
+# Create problem, major planning of searching all rooms of apartment
+# in most efficient pattern
+
+# store dimensions of apartment for distance calculation
+# store rooms and connections
+
--- python_app/vision/working_file.py
+++ python_app/vision/working_file.py
@@ -1,35 +1,66 @@
-import picamera
-from time import sleep
 
-camera = picamera.PiCamera()
+# How to solve for depth perception
+# move measured amount
+# focal length of camera
+# object/feature track
+# calculate depth of object(s)
 
-# All camera settings with their default values
-camera.sharpness = 0
-camera.contrast = 0
-camera.brightness = 50
-camera.saturation = 0
-camera.ISO = 0
-camera.video_stabilization = False
-camera.exposure_compensation = 0
-camera.exposure_mode = 'auto'
-camera.meter_mode = 'average'
-camera.awb_mode = 'auto'
-camera.image_effect = 'none'
-camera.color_effects = None
-camera.rotation = 0
-camera.hflip = False
-camera.vflip = False
-camera.crop = (0.0, 0.0, 1.0, 1.0)
+# Building profiles
+# of objects encountered
+# remember individuals (dog, human, cat, etc?)
+# breed of dog
+# give a unique name
+# api interaction allows settings based on individuals
 
-# Flip picture
-camera.hflip = True
-camera.vflip = True
+# Give it measurement (learning) ability
+# Place a mark on the floor a measured distance from the wall
+# Place two marks on the wall, one aligned with the floor mark
+# Use measure driving
+# Don't need to measure when using localization
 
-# Record video
-camera.start_recording('video.h264')
-sleep(5)
-camera.stop_recording()
+# Navigate apartment using localization
+# Plan moves to reach another room**
+# Distinguish between rooms
+# Find center of driving space with penalty for off planned path
 
-# Take picture
-## camera.capture('image.jpg')
+# Pups from Prometheus
+# 3D map and measure apartment
+# System for map
+# detect walls and objects, remember sequences of objects
+# remember relative positions of object
+
+# Hirearchy of planning algorithms
+# Planning small moves for navigation within a room
+# Planning larger moves for search
+
+# Reinforcement learning: ensure dog-stress-free operation
+# override plans when it knows a better move
+# introduce randomness to actions outside of plans
+# pass it features about it's environment
+# reward fewer moves
+# limit stress of dog
+
+# Simulated environment
+# teach agent to learn in simulation
+# limiting dog stress is main objective
+# monitoring dog is second
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
